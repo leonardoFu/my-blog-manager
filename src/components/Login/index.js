9c567fb72e2b0ad22c827f7066425eee8f1975db
leonardoFu <fuyh1@yonyou.com>
2017-08-29 20:34:58 +0800

    修改内容： 提交首页代码更改

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -9,32 +9,53 @@ import InputLabel from 'material-ui/Input/InputLabel';
 import FormControl from 'material-ui/Form/FormControl';
 import { FormControlLabel } from 'material-ui/Form';
 import Checkbox from 'material-ui/Checkbox';
-
+import background from '../../images/login-background.jpg';
 const styles = (theme) => ({
-  wrapper: {
+  'blur-back': {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    'background': `url(${background})`,
+    'background-size': 'cover',
+    transform:'scale(1.1)',
+    filter: 'blur(10px)',
+    zIndex: -1
+  },
+  'login-wrapper': {
     flex: '1 1 100%',
     display: 'flex',
+    position:'relative',
     minHeight: '100vh',
     justifyContent: 'center',
     alignItems: 'center',
-    background: lightBlue[100],
-    fontSize: '1rem'
+    'background-color': `rgba(179, 229, 252, 0.1)`,
+    fontSize: '1rem',
+    zIndex: 5,
   },
   formControl: {
     height: 50,
     margin: '15px auto',
   },
+  'home-icon': {
+    position:'relative',
+    fontSize: '2.5rem',
+    verticalAlign:'sub',
+  },
   form: {
     display:'flex',
+    position:'relative',
+    top: '-10%',
     width: 300,
-    height: 400,
-    alignItems: 'center',
+    height: 500,
+    alignItems: 'start',
     // background: cyan[50],
     borderRadius: 5,
     '& h1': {
       textAlign: 'center',
       fontWeight: 500
-    }
+    },
   }
 })
 class Login extends Component {
@@ -67,27 +88,33 @@ class Login extends Component {
   }
   render() {
     const { props: { classes } } = this;
-    return <div className = { classes.wrapper }>
-      <div className = { classes.form }>
-        <div>
-          <h1><Icon color = "primary">home</Icon><span>博客后台管理</span></h1>
-          <FormControl fullWidth className = { classes.formControl }>
-            <InputLabel htmlFor="username">用户名</InputLabel>
-            <Input id = "username" onChange = {this.changeUserName}></Input>
-          </FormControl>
-          <FormControl fullWidth className = { classes.formControl }>
-            <InputLabel htmlFor="password">密码</InputLabel>
-            <Input type = "password" id = "password" onChange = {this.changePassWord}></Input>
-          </FormControl>
-          <FormControlLabel
-            control={<Checkbox checked={this.state.rememberMe} onChange = {this.handleRemember}/>}
-            label="记住我">
-          </FormControlLabel>
-          <FormControl fullWidth className = { classes.formControl }>
-            <Button raised onClick = {this.handleLogin}> 登录</Button>
-          </FormControl>
+    return <div>
+      <div className = { classes['login-wrapper'] }>
+        <div className = { classes.form }>
+          <div>
+            <h1>
+              <span>Welcome</span>
+              <Icon color = "primary" className = { classes['home-icon'] }>home</Icon>
+            </h1>
+            <FormControl fullWidth className = { classes.formControl }>
+              <InputLabel htmlFor="username">用户名</InputLabel>
+              <Input id = "username" onChange = {this.changeUserName}></Input>
+            </FormControl>
+            <FormControl fullWidth className = { classes.formControl }>
+              <InputLabel htmlFor="password">密码</InputLabel>
+              <Input type = "password" id = "password" onChange = {this.changePassWord}></Input>
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox checked={this.state.rememberMe} onChange = {this.handleRemember}/>}
+              label="记住我">
+            </FormControlLabel>
+            <FormControl fullWidth className = { classes.formControl }>
+              <Button  onClick = {this.handleLogin}> 登录</Button>
+            </FormControl>
+          </div>
         </div>
       </div>
+      <div className = {classes['blur-back']}></div>
     </div>
   }
 }
